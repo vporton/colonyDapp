a058c91ce0d5638256320592544af17e94631647
Agnieszka Jarosik <agnieszka.jarosik-loj@pagepro.co>
2023-01-19 14:22:17 +0100

    added edition in the locked state

--- src/modules/pages/components/IncorporationPage/IncorporationPage.tsx
+++ src/modules/pages/components/IncorporationPage/IncorporationPage.tsx
@@ -1,12 +1,24 @@
 import React, { useCallback, useMemo, useRef, useState } from 'react';
-import { useParams } from 'react-router';
-import { Formik } from 'formik';
+import { RouteChildrenProps, useParams } from 'react-router';
+import { Formik, FormikErrors } from 'formik';
+import { defineMessages, FormattedMessage } from 'react-intl';
 
 import { useColonyFromNameQuery } from '~data/generated';
 import { getMainClasses } from '~utils/css';
 import { SpinnerLoader } from '~core/Preloaders';
 import Stages, { FormStages } from '~dashboard/Incorporation/Stages';
 import IncorporationForm from '~dashboard/Incorporation/IncorporationForm';
+import Stages, { FormStages } from '~dashboard/Incorporation/Stages';
+import LockedIncorporationForm from '~dashboard/Incorporation/IncorporationForm/LockedIncorporationForm';
+import { useDialog } from '~core/Dialog';
+import EditButtons from '~dashboard/ExpenditurePage/EditButtons/EditButtons';
+import EditIncorporationDialog from '~dashboard/Dialogs/EditIncorporationDialog';
+import Tag from '~core/Tag';
+import {
+  MotionStatus,
+  MotionType,
+} from '~dashboard/ExpenditurePage/Stages/constants';
+import { Motion } from '~pages/ExpenditurePage/types';
 
 import {
   initialValues,
@@ -14,9 +26,20 @@ import {
   validationSchema,
   Stages as StagesEnum,
 } from './constants';
-import styles from './IncorporationPage.css';
+import { findDifferences, updateValues } from './utils';
 import { ValuesType } from './types';
-import LockedIncorporationForm from '~dashboard/Incorporation/IncorporationForm/LockedIncorporationForm';
+import styles from './IncorporationPage.css';
+
+const MSG = defineMessages({
+  editMode: {
+    id: 'pages.IncorporationPage.editMode',
+    defaultMessage: `Edit mode. Changes require motion to pass.`,
+  },
+  title: {
+    id: 'pages.IncorporationPage.editMode',
+    defaultMessage: 'Incorporate this DAO',
+  },
+});
 
 const displayName = 'pages.IncorporationPage';
 
@@ -30,7 +53,16 @@ const IncorporationPage = () => {
   const [formValues, setFormValues] = useState<ValuesType>();
   const [shouldValidate, setShouldValidate] = useState(false);
   const [activeStageId, setActiveStageId] = useState(StagesEnum.Draft);
+  const [inEditMode, setInEditMode] = useState(false);
+  const oldValues = useRef<ValuesType>();
   const sidebarRef = useRef<HTMLElement>(null);
+  const [motion, setMotion] = useState<Motion>();
+
+  const openEditIncorporationDialog = useDialog(EditIncorporationDialog);
+
+  const { data: colonyData, loading } = useColonyFromNameQuery({
+    variables: { name: colonyName, address: '' },
+  });
 
   const handleSubmit = useCallback((values) => {
     setFormValues(values);
@@ -69,20 +101,83 @@ const IncorporationPage = () => {
     }
   }, [shouldValidate]);
 
-  const { data: colonyData, loading } = useColonyFromNameQuery({
-    variables: { name: colonyName, address: '' },
-  });
+  const handleConfirmEition = useCallback(
+    (confirmedValues: Partial<ValuesType> | undefined, isForced: boolean) => {
+      setInEditMode(false);
+      setFormEditable(false);
+
+      if (isForced) {
+        const data = updateValues(formValues, confirmedValues);
+        // call to backend to set new values goes here, setting state is temorary
+        setFormValues(data);
+      } else {
+        setMotion({ type: MotionType.Edit, status: MotionStatus.Pending });
+        // setTimeout is temporary, it should be replaced with call to api
+        setTimeout(() => {
+          const data = updateValues(formValues, confirmedValues);
+          setMotion({ type: MotionType.Edit, status: MotionStatus.Passed });
+          setFormValues(data);
+        }, 3000);
+      }
+    },
+    [formValues],
+  );
+
+  const handleEditLockedForm = useCallback(() => {
+    setInEditMode(true);
+    setFormEditable(true);
+    oldValues.current = formValues;
+  }, [formValues]);
+
+  const handleEditCancel = useCallback(() => {
+    setInEditMode(false);
+    setFormEditable(false);
+  }, []);
+
+  const handleEditSubmit = useCallback(
+    async (
+      values: ValuesType,
+      validateForm: (values?: ValuesType) => Promise<FormikErrors<ValuesType>>,
+    ) => {
+      setInEditMode(true);
+      setFormEditable(true);
+      const errors = await validateForm(values);
+      const hasErrors = Object.keys(errors)?.length;
+
+      const differentValues = findDifferences(values, oldValues.current);
+
+      return (
+        !hasErrors &&
+        colonyData &&
+        oldValues.current &&
+        openEditIncorporationDialog({
+          onSubmitClick: handleConfirmEition,
+          onCancelClick: handleEditCancel,
+          isVotingExtensionEnabled: true,
+          colony: colonyData?.processedColony,
+          newValues: differentValues,
+          oldValues: oldValues.current,
+        })
+      );
+    },
+    [
+      colonyData,
+      handleConfirmEition,
+      handleEditCancel,
+      openEditIncorporationDialog,
+    ],
+  );
 
   return isFormEditable ? (
     <Formik
-      initialValues={initialValues}
+      initialValues={formValues || initialValues}
       onSubmit={handleSubmit}
       validationSchema={validationSchema}
       validateOnBlur={shouldValidate}
       validateOnChange={shouldValidate}
       validate={handleValidate}
     >
-      {() => (
+      {({ values, validateForm }) => (
         <div className={getMainClasses({}, styles)}>
           <aside className={styles.sidebar} ref={sidebarRef}>
             {loading ? (
@@ -91,24 +186,35 @@ const IncorporationPage = () => {
               </div>
             ) : (
               colonyData && (
-                <IncorporationForm
-                  sidebarRef={sidebarRef.current}
-                  colony={colonyData.processedColony}
-                />
+                <>
+                  {inEditMode && (
+                    <div className={styles.tagWrapper}>
+                      <Tag>
+                        <FormattedMessage {...MSG.editMode} />
+                      </Tag>
+                    </div>
+                  )}
+                  <IncorporationForm
+                    sidebarRef={sidebarRef.current}
+                    colony={colonyData.processedColony}
+                  />
+                </>
               )
             )}
           </aside>
           <div className={styles.mainContainer}>
             <main className={styles.mainContent}>
-              <div />
-              {activeStageId === StagesEnum.Draft ? (
-                <FormStages activeStageId={activeStageId} stages={stages} />
-              ) : (
-                <Stages
-                  activeStageId={activeStageId}
-                  stages={stages}
-                  buttonAction={buttonAction}
+              <div className={styles.titleCommentsContainer}>
+                <FormattedMessage {...MSG.title} />
+              </div>
+              {inEditMode ? (
+                <EditButtons
+                  handleEditSubmit={() =>
+                    handleEditSubmit(values, validateForm)
+                  }
                 />
+              ) : (
+                <FormStages activeStageId={activeStageId} stages={stages} />
               )}
             </main>
           </div>
@@ -116,7 +222,7 @@ const IncorporationPage = () => {
       )}
     </Formik>
   ) : (
-    <div className={getMainClasses({}, styles)} id="expenditurePage">
+    <div className={getMainClasses({}, styles)}>
       <aside className={styles.sidebar} ref={sidebarRef}>
         {loading ? (
           <div className={styles.spinnerContainer}>
@@ -127,6 +233,7 @@ const IncorporationPage = () => {
           formValues && (
             <LockedIncorporationForm
               formValues={formValues}
+              editForm={handleEditLockedForm}
               activeStageId={activeStageId}
             />
           )
@@ -134,11 +241,14 @@ const IncorporationPage = () => {
       </aside>
       <div className={styles.mainContainer}>
         <main className={styles.mainContent}>
-          <div />
+          <div className={styles.titleCommentsContainer}>
+            <FormattedMessage {...MSG.title} />
+          </div>
           <Stages
             activeStageId={activeStageId}
             stages={stages}
             buttonAction={buttonAction}
+            motion={motion}
           />
         </main>
       </div>
